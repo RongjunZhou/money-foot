7c80fc4fb3d15dc428aa6867acf7a1068aefa12c
jun <zhourongjun1234@outlook.com>
2023-05-15 23:13:57 +0800

    fix:rename xml files

--- app/src/main/java/cn/edu/njupt/moneyfoot/ui/balance/BalanceFragment.kt
+++ app/src/main/java/cn/edu/njupt/moneyfoot/ui/balance/BalanceFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import cn.edu.njupt.moneyfoot.databinding.FragmentSlideshowBinding
+import cn.edu.njupt.moneyfoot.databinding.FragmentBalanceBinding
 
 class BalanceFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentBalanceBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class BalanceFragment : Fragment() {
         val balanceViewModel =
                 ViewModelProvider(this).get(BalanceViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentBalanceBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textSlideshow
