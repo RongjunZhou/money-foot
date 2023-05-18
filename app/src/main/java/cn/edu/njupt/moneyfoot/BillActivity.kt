de70991bea568d8291cebc90fe55d8b4dc080260
jun <zhourongjun1234@outlook.com>
2023-05-18 23:56:40 +0800

    feat:SQLite init

--- app/src/main/java/cn/edu/njupt/moneyfoot/BillActivity.kt
+++ app/src/main/java/cn/edu/njupt/moneyfoot/BillActivity.kt
@@ -1,24 +1,15 @@
 package cn.edu.njupt.moneyfoot
 
-import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
-import android.view.SurfaceControl.Transaction
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import cn.edu.njupt.moneyfoot.databinding.ActivityBillBinding
-import com.google.android.material.navigation.NavigationView
 
 class BillActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBillBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) : Unit {
         super.onCreate(savedInstanceState)
 
         binding = ActivityBillBinding.inflate(layoutInflater)
@@ -28,7 +19,6 @@ class BillActivity : AppCompatActivity() {
 
         binding.appBarBill.fab.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
-
             finish()
         }
 
