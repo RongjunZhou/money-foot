ea41b6b7a126901cbfc150461b817ea716066b69
jun <zhourongjun1234@outlook.com>
2023-05-22 12:02:07 +0800

    feat:响应式BillActivity，重定义应用名称

--- app/src/main/java/cn/edu/njupt/moneyfoot/BillActivity.kt
+++ app/src/main/java/cn/edu/njupt/moneyfoot/BillActivity.kt
@@ -1,13 +1,16 @@
 package cn.edu.njupt.moneyfoot
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import cn.edu.njupt.moneyfoot.databinding.ActivityBillBinding
 
 class BillActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBillBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +21,53 @@ class BillActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarBill.toolbar)
 
         binding.appBarBill.fab.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
+            onBackPressed()
             finish()
         }
 
         binding.appBarBill.toolbar.setNavigationOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            onBackPressed()
+        }
+
+        val typeSpinner = binding.appBarBill.content.typeSpinner
+        val accessSpinner = binding.appBarBill.content.accessSpinner
+
+        typeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (parent.getItemAtPosition(position).toString()) {
+                    "收入" -> {
+                        accessSpinner.adapter =
+                            ArrayAdapter(this@BillActivity,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                resources.getStringArray(R.array.tags_in))
+                    }
+                    "支出" -> {
+                        accessSpinner.adapter =
+                            ArrayAdapter(this@BillActivity,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                resources.getStringArray(R.array.tags_out))
+                    }
+                    else -> {
+                        accessSpinner.adapter =
+                            ArrayAdapter<String>(this@BillActivity,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                emptyArray())
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                accessSpinner.adapter =
+                    ArrayAdapter<String>(this@BillActivity,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        emptyArray())
+            }
+
         }
 
     }
