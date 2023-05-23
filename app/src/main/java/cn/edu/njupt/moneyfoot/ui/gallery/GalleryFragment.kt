158f70a7cbc70bfbe5986c522d448902ee984cb2
jun <zhourongjun1234@outlook.com>
2023-05-23 22:16:46 +0800

    feat:统计特性

--- app/src/main/java/cn/edu/njupt/moneyfoot/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/cn/edu/njupt/moneyfoot/ui/gallery/GalleryFragment.kt
@@ -1,13 +1,16 @@
 package cn.edu.njupt.moneyfoot.ui.gallery
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import cn.edu.njupt.moneyfoot.adapter.BillDOAdapter
 import cn.edu.njupt.moneyfoot.databinding.FragmentGalleryBinding
+import java.math.BigDecimal
 
 class GalleryFragment : Fragment() {
 
@@ -15,6 +18,7 @@ class GalleryFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -26,9 +30,27 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        galleryViewModel.all.observe(viewLifecycleOwner) {
+            var income = BigDecimal(0)
+            var outcome = BigDecimal(0)
+            it.stream().map { o -> BillDOAdapter.bill2BillDO(o) }.forEach{ o -> run{
+                when(o.tag){
+                    true -> income = income.add(o.amount)
+                    else -> outcome = outcome.add(o.amount)
+                }
+            }}
+            if(income.add(outcome).equals(0)){
+                binding.progressBar.progress = 100
+            }else{
+                binding.progressBar.progress = (income.toDouble()/(outcome.toDouble() + income.toDouble()) * 100).toInt()
+                if (income.toDouble() > outcome.toDouble()){
+                    binding.amount.setTextColor(Color.BLUE)
+                }else if(income.toDouble() > outcome.toDouble()){
+                    binding.amount.setTextColor(Color.RED)
+                }
+            }
+
+            binding.amount.text = income.toEngineeringString() + "/" + outcome.toEngineeringString()
         }
         return root
     }
--- app/src/main/java/cn/edu/njupt/moneyfoot/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/cn/edu/njupt/moneyfoot/ui/gallery/GalleryViewModel.kt
@@ -1,24 +1,19 @@
 package cn.edu.njupt.moneyfoot.ui.gallery
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import cn.edu.njupt.moneyfoot.repo.BillRepository
+import java.sql.Date
+import java.time.LocalDate
 import java.util.Calendar
-import java.util.Date
 
 class GalleryViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
-    }
-    val text: LiveData<String> = _text
-
     private val calendar: Calendar = Calendar.getInstance()
 
+
     val all = BillRepository.getBillsByDateAndAccess(
-        Date(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1,0),
-        Date(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1,0),
+        Date.valueOf(LocalDate.now().withDayOfMonth(1).toString()),
+        Date.valueOf(LocalDate.now().withDayOfMonth(1).plusMonths(1).toString()),
         null )
 
 
